dd64db428377cd2109f811a1183ebfd60cd50d75
ahmad90909090 <mubasshirahmad72@gmail.com>
2026-07-17 18:33:04 +0530

    Fix import path for Loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import Loader from "./components/Loader";
+import Loader from "./Components/Loader";
 import Home from "./pages/Home";
 import CreateRoom from "./pages/CreateRoom";
 import JoinRoom from "./pages/JoinRoom";
